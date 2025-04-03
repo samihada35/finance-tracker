4e5ed60444d9d6b75a22002357a9de84a38cfa88
Vitaly <vitasant35@gmail.com>
2025-04-03 21:32:07 +0200

    transaction console logic added

--- src/main/java/com/vitali/financetracker/Main.java
+++ src/main/java/com/vitali/financetracker/Main.java
@@ -4,27 +4,51 @@ import model.Transaction;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     private static List<Transaction> transactions = new ArrayList<Transaction>();
 
     public static void main(String[] args){
-        transactions.add(new Transaction("income", 1200.0, "salary"));
-        transactions.add(new Transaction("expense", 200.0, "Groceries"));
-        transactions.add(new Transaction("expense", 120.3, "Transport"));
 
-        double currentBalance = 0.0;
+        Scanner scanner = new Scanner(System.in);
 
-        for (Transaction tr:transactions){
-            System.out.println(tr);
-            if(tr.getType().equalsIgnoreCase("income")){
-                currentBalance+=tr.getAmount();
-            } else {
-                currentBalance-=tr.getAmount();
+        while (true){
+            System.out.println(
+                    "Choose an option:\n" +
+                    "1. Add transaction\n" +
+                    "2. Show all transactions\n" +
+                    "3. Show current balance\n" +
+                    "0. Exit"
+            );
+            int option = scanner.nextInt();
+            if (option == 1){
+                System.out.println("Type of transaction? income/expense");
+                String type = scanner.next();
+                System.out.println("Amount of transaction? example 22.0");
+                double amount = scanner.nextDouble();
+                System.out.println("Description of transaction? dollars/transport/fuel,etc");
+                String description = scanner.next();
+                transactions.add(new Transaction(type,amount,description));
+            } else if (option == 2){
+                for (Transaction tr:transactions){
+                    System.out.println(tr);
+                }
+            } else if (option == 3){
+                double currentBalance = 0;
+                for (Transaction tr:transactions){
+                    if(tr.getType().equalsIgnoreCase("income")){
+                        currentBalance+=tr.getAmount();
+                    } else {
+                        currentBalance-=tr.getAmount();
+                    }
+                }
+                System.out.println("Current balance: " + currentBalance);
+            }
+            if (option == 0){
+                break;
             }
         }
-        System.out.println("---");
-        System.out.println("Current balance: " + currentBalance);
     }
 }
