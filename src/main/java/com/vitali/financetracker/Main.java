c792c22c4b85b72cb0dc98b12b7820b25e8ed4a3
Vitaly <vitasant35@gmail.com>
2025-04-03 20:50:16 +0200

    ArrayList for storing objcts was added

--- src/main/java/com/vitali/financetracker/Main.java
+++ src/main/java/com/vitali/financetracker/Main.java
@@ -2,10 +2,29 @@ package com.vitali.financetracker;
 
 import model.Transaction;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    private static List<Transaction> transactions = new ArrayList<Transaction>();
+
     public static void main(String[] args){
-        Transaction tr = new Transaction("income",1200,"dollars");
-        System.out.println(tr);
-        System.out.println(tr.getAmount());
+        transactions.add(new Transaction("income", 1200.0, "salary"));
+        transactions.add(new Transaction("expense", 200.0, "Groceries"));
+        transactions.add(new Transaction("expense", 120.3, "Transport"));
+
+        double currentBalance = 0.0;
+
+        for (Transaction tr:transactions){
+            System.out.println(tr);
+            if(tr.getType().equalsIgnoreCase("income")){
+                currentBalance+=tr.getAmount();
+            } else {
+                currentBalance-=tr.getAmount();
+            }
+        }
+        System.out.println("---");
+        System.out.println("Current balance: " + currentBalance);
     }
 }
