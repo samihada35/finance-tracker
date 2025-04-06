5fcace6026d5cc36a5c8526d57667c6b9e1c3d46
Vitaly <vitasant35@gmail.com>
2025-04-06 16:01:31 +0200

    refactoring + StatisticServise was added and implemented

--- src/main/java/service/TransactionService.java
+++ src/main/java/service/TransactionService.java
@@ -2,43 +2,41 @@ package service;
 
 import model.Transaction;
 import model.TransactionType;
-import validators.InputValidation;
+import validators.Validators;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class TransactionService {
-    private static final List<Transaction> transactions = new ArrayList<Transaction>();
+    private static final List<Transaction> transactions = new ArrayList<>();
 
-    public void addTransaction(String type, double amount, String description){
-        InputValidation.validateTransaction(type,amount,description);
-        if(type.equalsIgnoreCase("income")){
-            transactions.add(new Transaction(TransactionType.INCOME,amount,description));
-        }
-        else if(type.equalsIgnoreCase("expense")){
-            transactions.add(new Transaction(TransactionType.EXPENSE,amount,description));
+    public void addTransaction(String type, double amount, String description) {
+        Validators.validateTransaction(type, amount, description);
+        if (type.equalsIgnoreCase("income")) {
+            transactions.add(new Transaction(TransactionType.INCOME, amount, description));
+        } else if (type.equalsIgnoreCase("expense")) {
+            transactions.add(new Transaction(TransactionType.EXPENSE, amount, description));
         }
     }
 
-    public static boolean deleteTransaction(int index){
-        InputValidation.validateIndex(index, transactions.size());
+    public static boolean deleteTransaction(int index) {
+        Validators.validateIndex(index, transactions.size());
         transactions.remove(index);
         return true;
     }
 
-    public List<Transaction> getAllTransactions(){
+    public List<Transaction> getAllTransactions() {
         return transactions;
     }
 
-    public double calculateBalance(){
+    public double calculateBalance() {
         double currentBalance = 0;
-        for (Transaction tr:transactions){
-            if(tr.getType().equals(TransactionType.INCOME)){
-                currentBalance+=tr.getAmount();
-            }
-            else if (tr.getType().equals(TransactionType.EXPENSE)) {
-                currentBalance-=tr.getAmount();
+        for (Transaction tr : transactions) {
+            if (tr.getType().equals(TransactionType.INCOME)) {
+                currentBalance += tr.getAmount();
+            } else if (tr.getType().equals(TransactionType.EXPENSE)) {
+                currentBalance -= tr.getAmount();
             }
         }
         return currentBalance;
